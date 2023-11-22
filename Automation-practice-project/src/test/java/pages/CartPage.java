530fec914b131d24a64ac553f83e682daf41c259
Lukasz <lukaszkunecki7@gmail.com>
2023-11-22 23:51:18 +0100

    -

--- Automation-practice-project/src/test/java/pages/CartPage.java
+++ Automation-practice-project/src/test/java/pages/CartPage.java
@@ -1,4 +1,16 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CartPage {
+
+    public CartPage(WebElement driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(className = "shopping_cart")
+    WebElement cart;
+
 }
