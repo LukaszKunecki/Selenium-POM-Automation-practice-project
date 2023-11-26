c79315a587eb6b3a40bff18aed9747db7d39bdec
Lukasz <lukaszkunecki7@gmail.com>
2023-11-26 21:53:33 +0100

    -

--- Automation-practice-project/src/test/java/pages/TopBarPage.java
+++ Automation-practice-project/src/test/java/pages/TopBarPage.java
@@ -1,6 +1,8 @@
 package pages;
 
+import org.junit.jupiter.api.ClassOrderer;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class TopBarPage {
@@ -8,4 +10,10 @@ public class TopBarPage {
     public TopBarPage(WebElement driver) {
         PageFactory.initElements(driver, this);
     }
+
+    @FindBy(id = "contact-link")
+    WebElement contact;
+
+    @FindBy(className = "header_user_info")
+    WebElement signIn;
 }
